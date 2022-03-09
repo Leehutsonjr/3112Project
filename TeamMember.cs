1383c5b1879090961990381e6f84e9a6ecfa3301
Torkandus <77407197+Torkandus@users.noreply.github.com>
2022-03-09 13:10:26 -0500

    Initial Upload
    Two classes - Project and TeamMember. Project contains a list of TeamMember objects.

---
+++ TeamMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintCompass
+{
+    public class TeamMember
+    {
+        private string name { get; }
+        public TeamMember(string name)
+        {
+            this.name = name;
+        }
+    }
+}
